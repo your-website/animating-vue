133252a4b4008d1840ce25f22247186533f5173a
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-13 11:21:29 +0700

    add group transitions

--- src/router.js
+++ src/router.js
@@ -21,6 +21,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/list",
+      name: "list",
+      component: () =>
+        import("./views/List.vue")
     }
   ]
 });
